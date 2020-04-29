ec2b3bb1dec58d1d0c278622ade337b9e9624848
Sobira <sobira204@gmail.com>
2020-04-29 21:00:56 +0500

    add method update and select

--- Program.cs
+++ Program.cs
@@ -10,9 +10,13 @@ namespace HomeWork_29_04
         static List<Client> ClientsList;
         static void Main(string[] args)
         {
+            Thread insertThread = new Thread( new ParameterizedThreadStart (Insert));
+            Thread selectThread = new Thread(new ParameterizedThreadStart(Select));
+            Thread deleteThread = new Thread(new ParameterizedThreadStart(Delete));
+            Thread updateThread = new Thread(new ParameterizedThreadStart(Update));
 
         }
-        public void Insert(object obj){
+        public static void Insert(object obj){
             lock(locker){
                 ClientsList.Add((Client)obj);
                 Console.ForegroundColor = ConsoleColor.Green;
@@ -20,7 +24,7 @@ namespace HomeWork_29_04
                 Console.ForegroundColor = ConsoleColor.White;
             }
         }
-        public void Select(object obj){
+        public static void SelectById(object obj){
             lock(locker){
                 foreach(var client in ClientsList){
                     if((int)obj == client.Id){
@@ -38,15 +42,44 @@ namespace HomeWork_29_04
                 }
             }
         }
-        public void Delete(object obj){
+        public static void Select(object obj){
+            lock(locker){
+                foreach(var client in ClientsList){                    
+                    System.Console.WriteLine(client.Id);
+                    System.Console.WriteLine(client.Firstname);
+                    System.Console.WriteLine(client.Middlename);
+                    System.Console.WriteLine(client.Lastname);
+                    System.Console.WriteLine(client.Balance);
+                }
+            }
+        }
+        public static void Delete(object obj){
             lock(locker){
                 foreach(var client in ClientsList){
                     if((int)obj == client.Id){
                         ClientsList.Remove(client);
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        System.Console.WriteLine("Client with such id was removed");
+                        Console.ForegroundColor = ConsoleColor.White;
                     }
                 }
             }
         }
-        
+        public static void Update(object obj){
+            lock(locker){
+                foreach(var client in ClientsList){
+                    if((int)obj == client.Id){
+                        System.Console.Write("enter firstname: ");
+                        client.Firstname = Console.ReadLine();
+                        System.Console.Write("enter middlename: ");
+                        client.Middlename = Console.ReadLine();
+                        System.Console.Write("enter lastname: ");
+                        client.Lastname = Console.ReadLine();
+                        System.Console.Write("enter balance: ");
+                        client.Balance = decimal.Parse(Console.ReadLine());
+                    }
+                }
+            }
+        }
     }
 }
